dd96fdc7ba2476db03f785e23439143e418323d9
jiakang.peng <pjkbalance@163.com>
2018-08-19 21:08:15 +0800

    20180819

---
+++ src/cn/jk/study/spring8/service/UserService.java
@@ -0,0 +1,11 @@
+package cn.jk.study.spring8.service;
+
+/**
+ * Created by jiakang.peng on 2018/8/19.
+ */
+public interface UserService {
+    public int add();
+    public void update(int id);
+    public void delete(int id);
+
+}
